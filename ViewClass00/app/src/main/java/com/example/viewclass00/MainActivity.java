fd6485c1af87f6fcef3cbda7cd62af31e180a033
Heon_9u <frtt0608@naver.com>
2020-11-30 18:39:18 +0900

    201130|ViewClass00|View클래스로 onDraw 활용

--- ViewClass00/app/src/main/java/com/example/viewclass00/MainActivity.java
+++ ViewClass00/app/src/main/java/com/example/viewclass00/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.viewclass00;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Paint p1 = new Paint();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MyView myview = new MyView(this);
+        setContentView(myview);
+    }
+
+    public class MyView extends View {
+        public MyView(Context context) {
+            super(context);
+            p1.setColor(Color.RED);
+            p1.setTextSize(40);
+        }
+
+        public void onDraw(Canvas canvas) {
+            canvas.drawCircle(100, 100, 100, p1);
+            canvas.drawRect(0, 300, 300, 400, p1);
+            canvas.drawText("View 클래스를 상속받고 onDraw 활용하기", 0, 500, p1);
+        }
     }
 }
