588e4452bb9bed9dd4256371df39eebb3551fe18
Heon_9u <frtt0608@naver.com>
2020-11-30 16:20:45 +0900

    201130|Intent를 활용한 화면 전환

--- IntentTest00/app/src/main/java/com/example/intenttest00/ShowMe.java
+++ IntentTest00/app/src/main/java/com/example/intenttest00/ShowMe.java
@@ -1,5 +1,4 @@
 package com.example.intenttest00;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,21 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowMe extends AppCompatActivity {
     Button b1;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sub1);
 
         b1 = (Button) findViewById(R.id.b1);
-
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(getApplicationContext(), ShowMe.class);
-                startActivity((intent1));
+                finish();
             }
         });
     }
