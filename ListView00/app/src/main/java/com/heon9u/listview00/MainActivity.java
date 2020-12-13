ce3901f94bbe14ce70d87c7ce98b8b65912870e1
Heon_9u <frtt0608@naver.com>
2020-12-13 23:48:30 +0900

    201213|ListView|ListActivity를 사용해 ListView 만들기

--- ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
+++ ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
@@ -1,35 +1,40 @@
 package com.heon9u.listview00;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-import static android.R.layout.simple_list_item_1;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+
+
+public class MainActivity extends ListActivity {
+
+    ArrayList<String> people;
+    String[] explain = {"조선 중기의 명장", "거란의 침략을 막아냄", "훈민정음 창제",
+                        "일제강점기의 독립 운동가", "이쁜 공주1", "이쁜 공주2"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        final String[] listExplain = getResources().getStringArray(R.array.achievement);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
-                R.array.people, simple_list_item_1);
+        people = new ArrayList<>();
+        people.add("이순신");
+        people.add("강감찬");
+        people.add("세종대왕");
+        people.add("윤봉길");
+        people.add("조단비");
+        people.add("조은비");
 
-        ListView listview = (ListView) findViewById(R.id.ListView);
-        listview.setAdapter(adapter);
+        ArrayAdapter<String> mAdapter;
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, people);
+        setListAdapter(mAdapter);
+    }
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) view).getText() + listExplain[i], Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onListItemClick(ListView lv, View v, int position, long id) {
+        String message = people.get(position) + ": " + explain[position];
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 }
