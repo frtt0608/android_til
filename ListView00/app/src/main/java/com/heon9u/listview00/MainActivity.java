9a43d15d5fffee6501e38d7fbb44a89dae922dd5
Heon_9u <frtt0608@naver.com>
2020-12-13 18:22:38 +0900

    201213|ListView 기본 골격 구성하기

--- ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
+++ ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
@@ -3,6 +3,8 @@ package com.heon9u.listview00;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] list = {"이순신", "광개토대왕", "신사임당", "세종대왕", "이성계", "윤봉길",
+                        "안중근", "을지문덕", "강감찬", "이황", "이이", "장영실", "에디슨",
+                        "슈바이처", "모짜르트", "나폴레옹", "링컨", "공자", "맹자"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        ListView listview = (ListView) findViewById(R.id.ListView);
+
+        listview.setAdapter(adapter);
     }
 }
