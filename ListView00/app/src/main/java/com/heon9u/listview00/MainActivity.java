7d0f107a40c4a3ca3dec38ef6a5bfd2f0ebba472
Heon_9u <frtt0608@naver.com>
2020-12-13 23:40:59 +0900

    201213|ListView|java에 있는 데이터를 xml파일에서 사용하기

--- ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
+++ ListView00/app/src/main/java/com/heon9u/listview00/MainActivity.java
@@ -1,27 +1,35 @@
 package com.heon9u.listview00;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import static android.R.layout.simple_list_item_1;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] list = {"이순신", "광개토대왕", "신사임당", "세종대왕", "이성계", "윤봉길",
-                        "안중근", "을지문덕", "강감찬", "이황", "이이", "장영실", "에디슨",
-                        "슈바이처", "모짜르트", "나폴레옹", "링컨", "공자", "맹자"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, list);
+        final String[] listExplain = getResources().getStringArray(R.array.achievement);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.people, simple_list_item_1);
 
         ListView listview = (ListView) findViewById(R.id.ListView);
-
         listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText() + listExplain[i], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
