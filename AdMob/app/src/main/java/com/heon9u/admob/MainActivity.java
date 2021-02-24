1a560aa98e4912541ce9ffc61574a4b251b8d3e2
Heon_9u <frtt0608@naver.com>
2021-02-25 00:09:38 +0900

    210225 | AdMob 구현 중...|servicebinding error와 EGL, cookie_manager 이슈 발생

--- AdMob/app/src/main/java/com/heon9u/admob/MainActivity.java
+++ AdMob/app/src/main/java/com/heon9u/admob/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileAds.initialize(this, initializationStatus -> { });
+
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
     }
+
 }
