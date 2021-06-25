bb41c3c099ee6d5dab1e52c2748a25ca33795d9f
Heon_9u <frtt0608@naver.com>
2021-06-25 23:07:17 +0900

    210625 | RecyclerView Layout|ViewModel의 observe로 note객체의 변화 적용

--- AACproject/app/src/main/java/com/heon9u/aacproject/MainActivity.java
+++ AACproject/app/src/main/java/com/heon9u/aacproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.heon9u.aacproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,12 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 // update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+
+                adapter.setNotes(notes);
             }
         });
     }
